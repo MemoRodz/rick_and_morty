11d03fe4bed0efaa89b65051e6d4c35473b814c1
MemoRodz <gmo.rodriguez@gmail.com>
2023-02-10 00:59:19 -0600

    Try fixed compilation error in file Cards.jsx *React* must be in scope when using JSX

--- front/src/components/Card.jsx
+++ front/src/components/Card.jsx
@@ -5,48 +5,48 @@ import { useDispatch, useSelector } from "react-redux";
 import { addFavorites, deleteFavorites } from "../Redux/actions";
 
 export default function Card(props) {
-   const [isFav, setIsFav] = useState(false);
-   const dispatch = useDispatch();
-   const myFavorites = useSelector((s) => s.myFavorites);
+  const [isFav, setIsFav] = useState(false);
+  const dispatch = useDispatch();
+  const myFavorites = useSelector((s) => s.myFavorites);
 
-   function handleFavorite(ch) {
-      if (isFav) {
-         setIsFav(false);
-         dispatch(deleteFavorites(ch.id));
-      } else {
-         setIsFav(true);
-         dispatch(addFavorites(ch));
+  function handleFavorite(ch) {
+    if (isFav) {
+      setIsFav(false);
+      dispatch(deleteFavorites(ch.id));
+    } else {
+      setIsFav(true);
+      dispatch(addFavorites(ch));
+    }
+  }
+
+  useEffect(() => {
+    myFavorites.forEach((ch) => {
+      if (ch.id === props.id) {
+        setIsFav(true);
       }
-   }
-
-   useEffect(() => {
-      myFavorites.forEach((ch) => {
-         if (ch.id === props.id) {
-            setIsFav(true);
-         }
-      });
-   }, [myFavorites]);
+    });
+  }, [myFavorites]);
 
    return (
-      <div className={styles.card}>
-         <div className={styles.upbar_card}>
-            {isFav ? (
-               <button onClick={() => handleFavorite(props)}>❤️</button>
-            ) : (
-               <button onClick={() => handleFavorite(props)}>🤍</button>
-            )}
-            <button className={styles.bttn} onClick={props.onClose}>
-               X
-            </button>
-         </div>
-         <div className={styles.txt}>
-            <Link className={styles.linki} to={`/detail/${props.id}`}>
-               <h2>{props.name}</h2>
-               <p>{props.species}</p>
-               <p>{props.gender}</p>
-               <img src={props.image} alt={props.image} />
-            </Link>
-         </div>
+    <div className={styles.card}>
+      <div className={styles.upbar_card}>
+        {isFav ? (
+          <button onClick={() => handleFavorite(props)}>❤️</button>
+        ) : (
+          <button onClick={() => handleFavorite(props)}>🤍</button>
+        )}
+        <button className={styles.bttn} onClick={props.onClose}>
+          X
+        </button>
       </div>
-   );
+      <div className={styles.txt}>
+        <Link className={styles.linki} to={`/detail/${props.id}`}>
+          <h2>{props.name}</h2>
+          <p>{props.species}</p>
+          <p>{props.gender}</p>
+          <img src={props.image} alt={props.image} />
+        </Link>
+      </div>
+    </div>
+  );
 }
